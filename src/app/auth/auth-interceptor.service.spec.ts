27c36692f89fa9d30bc8f746deb2ea6a3f2b2ce9
Kyla Marczewski <marczk@uw.edu>
2019-09-25 17:02:03 -0700

    auth interceptor testing

--- src/app/auth/auth-interceptor.service.spec.ts
+++ src/app/auth/auth-interceptor.service.spec.ts
@@ -3,39 +3,92 @@ import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
-import { SquacApiService } from '../squacapi.service';
 import { AuthInterceptorService } from './auth-interceptor.service';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { Observable, of, BehaviorSubject } from 'rxjs';
+import { User } from './user';
+import { Data } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+class MockAuthService {
+  user = new BehaviorSubject<User>(null);
+
+  logIn() {
+    this.user.next(new User(
+      "email",
+      "token",
+      new Date()
+    ));
+  }
+
+  logOut() {
+    this.user.next(null);
+  }
+
+}
 
 describe(`AuthInterceptor`, () => {
-  let service: SquacApiService;
-  let httpMock: HttpTestingController;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let testUrl = "data/";
+  let service;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
-        SquacApiService,
+        { provide : AuthService, useValue : new MockAuthService() },
         {
           provide: HTTP_INTERCEPTORS,
           useClass: AuthInterceptorService,
           multi: true,
-        },
-      ],
+        }
+      ]
+      
     });
 
-    service = TestBed.get(SquacApiService);
-    httpMock = TestBed.get(HttpTestingController);
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(AuthService);
   });
 
-  it('should add an Authorization header', () => {
-    // service.getPosts().subscribe(response => {
-    //   expect(response).toBeTruthy();
-    // });
-  
-    // const httpRequest = httpMock.expectOne(`${service.ROOT_URL}/posts`);
-  
-    // expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
+  afterEach(()=>{
+    httpTestingController.verify();
   });
-  
+
+  it('should add an Authorization header if user logged in', () => {
+    service.logIn();
+      
+      // Make an HTTP GET request
+    httpClient.get("https://test.test.test/")
+      .subscribe(response => {
+        expect(response).toBeTruthy
+      }
+    );
+    const httpRequest = httpTestingController.expectOne("https://test.test.test/");
+
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(true);   
+
+    // expect(httpRequest.request.headers.get('Authorization')).toBe(
+    //   'Token token',
+    // );
+  });
+
+  it('should not add an Authorization header if user not logged in', () => {
+    service.logOut();
+      
+      // Make an HTTP GET request
+    httpClient.get("https://test.test.test/")
+      .subscribe(response => {
+        expect(response).toBeTruthy
+      }
+    );
+
+    const httpRequest = httpTestingController.expectOne("https://test.test.test/");
+
+    expect(httpRequest.request.headers.has('Authorization')).toEqual(false);
+  });  
+
 });
