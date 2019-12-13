94ac48a59868604738532a38453b238df6436efe
jtclayt <jt.clayton92@yahoo.com>
2019-12-13 11:44:45 -0800

    Styling fixes for buttons in headers, reduced tags for selecttion colors, added an edit button to metric and channel group pages.

--- src/app/metrics/metrics-view/metrics-view.component.ts
+++ src/app/metrics/metrics-view/metrics-view.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Metric } from '../../shared/metric';
 import { MetricsService } from '../../shared/metrics.service';
-import { ColumnMode } from '@swimlane/ngx-datatable';
+import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
 
 @Component({
   selector: 'app-metrics-view',
@@ -13,9 +13,12 @@ import { ColumnMode } from '@swimlane/ngx-datatable';
 export class MetricsViewComponent implements OnInit, OnDestroy {
   metrics: Metric[];
   subscription: Subscription;
+  selectedMetric: Metric;
+  selected = false;
 
   // Table stuff
   ColumnMode = ColumnMode;
+  SelectionType = SelectionType;
 
   constructor(
     private router: Router,
@@ -39,5 +42,15 @@ export class MetricsViewComponent implements OnInit, OnDestroy {
     this.router.navigate(['new'], {relativeTo: this.route});
   }
 
+  editMetric() {
+    this.router.navigate([`${this.selectedMetric.id}/edit`], {relativeTo: this.route});
+  }
+
+  onSelect($event) {
+    this.selected = true;
+    this.metricsService.getMetric($event.selected[0].id).subscribe(metric => {
+      this.selectedMetric = metric;
+    });
+  }
 }
 
