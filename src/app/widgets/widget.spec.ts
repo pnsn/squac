7a80e8fd9e92995245b14c8577b635326e153041
Kyla Marczewski <marczk@uw.edu>
2020-03-05 13:47:55 -0800

    last of the mocks

--- src/app/widgets/widget.spec.ts
+++ src/app/widgets/widget.spec.ts
@@ -4,4 +4,10 @@ describe('Widget', () => {
   it('should create an instance', () => {
     expect(new Widget(1, 'name', 'description', 1, 1, 1, 1, 1, 1, 1, [])).toBeTruthy();
   });
+
+  it('should return empty array no metrics', () => {
+    const widget = new Widget(1, 'name', 'description', 1, 1, 1, 1, 1, 1, 1, [])
+    
+    expect(widget.metricsIds).toEqual([]);
+  })
 });
