28afd6c1c0a8ff63f33b1702a0e775b50bddb03b
Kyla Marczewski <marczk@uw.edu>
2020-03-12 15:43:58 -0700

    changing versioning

--- src/app/squacapi.service.ts
+++ src/app/squacapi.service.ts
@@ -10,19 +10,17 @@ import { Injectable } from '@angular/core';
 // Generic class for interacting with squac api
 export class SquacApiService {
   protected baseUrl: string;
-  protected version: string;
 
   constructor(
     private http: HttpClient
   ) {
-    this.baseUrl = environment.apiUrl;
-    this.version = environment.version;
+    this.baseUrl = environment.apiUrl + environment.version;
   }
   // TODO test
 
   // http get with optional id & params
   get( path: string , id ?: number, params?: Params ): Observable<any> {
-    const url = this.getBaseUrl(path) + (id ? id : '');
+    const url = this.baseUrl + path + (id ? id : '');
     return this.http.get<any>( url , {
       params
     });
@@ -30,32 +28,22 @@ export class SquacApiService {
 
   // http post with data
   post(path: string, data: any): Observable<any> {
-    const url = this.getBaseUrl(path);
+    const url = this.baseUrl + path;
     console.log('post: ', url);
     return this.http.post<any>(url, data );
   }
 
   // for updating
   put(path: string, id: number, data: any ): Observable<any> {
-    const url = this.getBaseUrl(path) + (id ? id + '/' : '');
+    const url = this.baseUrl + path + (id ? id + '/' : '');
     console.log('put: ', url);
     return this.http.put<any>(url, data);
   }
 
   // for deleting
   delete(path: string, id: number) {
-    const url = this.getBaseUrl(path) + (id ? id + '/' : '');
+    const url = this.baseUrl + path + (id ? id + '/' : '');
     return this.http.delete<any>(url);
-  }
-
-  // all routes except user are versioned
-  private getBaseUrl(path: string): string {
-    let url: string;
-    url = this.baseUrl;
-    if (!path.includes('user')) {
-      url += this.version;
-    }
-    return url + path;
   }
 
 }
