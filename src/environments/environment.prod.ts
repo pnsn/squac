6fca45d4ed1b9ae09109056136643d7381319270
Kyla Marczewski <marczk@uw.edu>
2020-05-18 16:46:39 -0700

    Remove staging

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://squacapi.pnsn.org/',
+  apiUrl: 'https://staging-squacapi.pnsn.org/',
   version: 'v1.0/'
 };
--- src/environments/environment.staging.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://staging-squacapi.pnsn.org/',
-  version: 'v1.0/'
-};
