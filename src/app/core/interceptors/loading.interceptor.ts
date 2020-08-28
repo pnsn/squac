30bc22deec7244c5f056df7bcd877af6a37527a6
Kyla Marczewski <marczk@uw.edu>
2020-08-28 16:27:10 -0700

    channel groups

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -17,7 +17,8 @@ export class LoadingInterceptor implements HttpInterceptor {
    * URLs for which the loading screen should not be enabled
    */
   skipUrls = [
-    'measurements'
+    'measurements',
+    'channels'
   ];
 
   constructor(
