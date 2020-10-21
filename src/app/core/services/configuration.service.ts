39400f90270bbe23a2abc3f56a42b06d46b24a55
Kyla Marczewski <marczk@uw.edu>
2020-10-21 16:52:41 -0700

    config service

---
+++ src/app/core/services/configuration.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { mapTo, tap } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ConfigurationService {
+  private configuration = {};
+
+  constructor(private httpClient: HttpClient) {
+    console.log("create config")
+  }
+
+  load(): Observable<void> {
+    return this.httpClient.get('/assets/config.json')
+      .pipe(
+        tap((configuration: any) => this.configuration = configuration),
+        mapTo(undefined),
+      );
+  }
+
+  getValue(key: string, defaultValue?: any): any {
+    return this.configuration[key] || defaultValue;
+  }
+}
