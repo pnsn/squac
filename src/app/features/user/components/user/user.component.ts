38903d331d75abe93be919b2134c700dc04702f6
Kyla Marczewski <marczk@uw.edu>
2020-10-30 15:19:44 -0700

    user informaiton

--- src/app/features/user/components/user/user.component.ts
+++ src/app/features/user/components/user/user.component.ts
@@ -7,6 +7,7 @@ import { OrganizationsService } from '@features/user/services/organizations.serv
 import { Organization } from '@features/user/models/organization';
 import { switchMap } from 'rxjs/operators';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MessageService } from '@core/services/message.service';
 
 @Component({
   selector: 'app-user',
@@ -23,7 +24,8 @@ export class UserComponent implements OnInit, OnDestroy {
   constructor(
     private route: ActivatedRoute,
     private userService: UserService,
-    private orgService: OrganizationsService
+    private orgService: OrganizationsService,
+    private messageService: MessageService
   ) { }
 
   ngOnInit() {
@@ -51,8 +53,7 @@ export class UserComponent implements OnInit, OnDestroy {
         user.firstName,
         Validators.required
         ),
-      lastName: new FormControl(user.lastName, Validators.required),
-      email: new FormControl(user.email, [Validators.required, Validators.email])
+      lastName: new FormControl(user.lastName, Validators.required)
     });
   }
 
@@ -66,8 +67,10 @@ export class UserComponent implements OnInit, OnDestroy {
       user => {
         this.userService.fetchUser();
         this.editMode = false;
+        this.messageService.message("User information updated.");
       },
       error => {
+        this.messageService.error("Could not save user information.");
         console.log('error in change user: ', error);
       }
     );
