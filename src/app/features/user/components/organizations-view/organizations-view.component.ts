7aef4b37115929754ee9a60b8df52122649e657c
Kyla Marczewski <marczk@uw.edu>
2020-11-09 14:56:33 -0800

    add org view component

---
+++ src/app/features/user/components/organizations-view/organizations-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organizations-view',
+  templateUrl: './organizations-view.component.html',
+  styleUrls: ['./organizations-view.component.scss']
+})
+export class OrganizationsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
