d497365a7aa958ff3154266571f5f815742bfa14
Kyla Marczewski <marczk@uw.edu>
2020-11-25 17:47:33 -0800

    adding service stuff

--- src/app/features/alarms/services/alarms.service.ts
+++ src/app/features/alarms/services/alarms.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { SquacApiService } from '@core/services/squacapi.service';
+import { map, tap } from 'rxjs/operators';
+import { Alarm } from '../models/alarm';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlarmsService {
 
-  private url = 'alarms/metrics/';
+  private url = 'measurement/alarm/';
 
 
   constructor(
@@ -14,9 +16,51 @@ export class AlarmsService {
   ) {}
   
   getAlarms() {
-    this.squacApi.get()
+    return this.squacApi.get(this.url).pipe(
+      map(
+        results => {
+          const alarms: Alarm[] = [];
+
+          results.forEach(alarm => {
+            alarms.push(this.mapAlarm(alarm));
+          });
+          return alarms;
+        }
+      )
+    );
   }
- 
+  
+  getAlarm(id: number) {
+    return this.squacApi.get(this.url, id).pipe(
+      map(
+        response => {
+          return this.mapAlarm(response);
+        }
+      )
+    )
+  }
+
+    // Replaces channel group with new channel group
+    updateAlarm(alarm: Alarm) {
+      const postData = {
+        id: 1
+      };
+
+      if (alarm.id) {
+        postData.id = alarm.id;
+        return this.squacApi.put(this.url, alarm.id, postData).pipe(map(
+          response => this.mapAlarm(response)
+        ));
+      }
+      return this.squacApi.post(this.url, postData).pipe(map(
+        response => this.mapAlarm(response)
+      ));
+    }
+
+  mapAlarm(alarm) {
+
+    return alarm;
+  }
   //getAlarms
 
   //getAlarm
--- src/app/features/alarms/services/alerts.service.ts
+++ src/app/features/alarms/services/alerts.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import { SquacApiService } from '@core/services/squacapi.service';
+import { map } from 'rxjs/operators';
+import { Alert } from '../models/alert';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlertsService {
 
-  constructor() { }
+  private url = 'measurement/alert/';
+
+
+  constructor(
+    private squacApi: SquacApiService
+  ) {}
+  
+  getAlerts() {
+    return this.squacApi.get(this.url).pipe(
+      map(
+        results => {
+          const alerts: Alert[] = [];
+
+          results.forEach(alert => {
+            alerts.push(this.mapAlert(alert));
+          });
+          return alerts;
+        }
+      )
+    );
+  }
+  
+  getAlert(id: number) {
+    return this.squacApi.get(this.url, id).pipe(
+      map(
+        response => {
+          return this.mapAlert(response);
+        }
+      )
+    )
+  }
+
+    // Replaces channel group with new channel group
+    updateAlert(alert: Alert) {
+      const postData = {
+        id: 1
+      };
+
+      if (alert.id) {
+        postData.id = alert.id;
+        return this.squacApi.put(this.url, alert.id, postData).pipe(map(
+          response => this.mapAlert(response)
+        ));
+      }
+      return this.squacApi.post(this.url, postData).pipe(map(
+        response => this.mapAlert(response)
+      ));
+    }
+
+  mapAlert(alert) {
+
+    return alert;
+  }
 }
