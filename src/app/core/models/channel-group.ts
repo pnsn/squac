f1256c732fd39429c28891af292a0920874c2a21
Kyla Marczewski <marczk@uw.edu>
2021-03-04 16:43:25 -0800

    channel group adapter

--- src/app/core/models/channel-group.ts
+++ src/app/core/models/channel-group.ts
@@ -1,4 +1,7 @@
+import { Injectable } from '@angular/core';
+import { Adapter } from './adapter';
 import { Channel } from './channel';
+import { Metric } from './metric';
 
 // Describes a channel group object
 export class ChannelGroup {
@@ -18,5 +21,38 @@ export class ChannelGroup {
 
   get channelsString(): string {
     return this.channelIds.toString();
+  }
+}
+
+
+export interface apiGetChannelGroup {
+  name: string;
+  id: number;
+  url: string;
+  description: string;
+  channels: Array<number | any>;
+  share_all: boolean;
+  share_org: boolean;
+  created_at: string;
+  updated_at: string;
+  user_id: string;
+  organization: number;
+}
+
+@Injectable({
+  providedIn: "root",
+})
+export class ChannelGroupAdapter implements Adapter<ChannelGroup> {
+  adapt(item: apiGetChannelGroup): ChannelGroup {
+    return new ChannelGroup(
+      item.id,
+      +item.user_id,
+      item.name,
+      item.description,
+      item.organization,
+      item.share_org,
+      item.share_all,
+      item.channels
+    );
   }
 }
