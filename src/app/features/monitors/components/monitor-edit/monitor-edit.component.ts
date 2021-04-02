2056c81560c316df5544fd9a9a5d0f2f98db6f03
Kyla Marczewski <marczk@uw.edu>
2021-04-02 12:27:57 -0700

    remove consloe

--- src/app/features/monitors/components/monitor-edit/monitor-edit.component.ts
+++ src/app/features/monitors/components/monitor-edit/monitor-edit.component.ts
@@ -88,8 +88,6 @@ export class MonitorEditComponent implements OnInit, OnDestroy {
         level: null,
        inclusive: null
       });
-
-      console.log(this.triggers.at(index).value)
     } else {
       this.triggers.removeAt(index);
     }
