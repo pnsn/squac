4f40ef828676bfb0beb336635918727ded4ac0b5
Kyla Marczewski <marczk@uw.edu>
2021-12-08 13:43:03 -0800

    filtering on channel group edit

--- src/app/features/widgets/components/widget-edit/channel-groups-edit/channel-groups-edit.component.ts
+++ src/app/features/widgets/components/widget-edit/channel-groups-edit/channel-groups-edit.component.ts
@@ -4,6 +4,11 @@ import { ChannelGroupsService } from '@features/channel-groups/services/channel-
 import { WidgetEditService } from '../../../services/widget-edit.service';
 import { Subscription } from 'rxjs';
 import { ColumnMode, SelectionType } from '@swimlane/ngx-datatable';
+import { Organization } from '@features/user/models/organization';
+import { OrganizationPipe } from '@shared/pipes/organization.pipe';
+import { OrganizationsService } from '@features/user/services/organizations.service';
+import { UserService } from '@features/user/services/user.service';
+import { UserPipe } from '@shared/pipes/user.pipe';
 
 @Component({
   selector: 'app-channel-groups-edit',
@@ -21,10 +26,22 @@ export class ChannelGroupsEditComponent implements OnInit, OnDestroy, AfterViewI
   ColumnMode = ColumnMode;
   SelectionType =  SelectionType;
   done = false;
+  showOnlyUserOrg = true;
+  userOrg: Organization;
+  temp;
+  columns;
+  userPipe: UserPipe;
+  orgPipe: OrganizationPipe;
+
   constructor(
     private channelGroupsService: ChannelGroupsService,
     private widgetEditService: WidgetEditService,
-  ) { }
+    private userService: UserService,
+    private orgService: OrganizationsService
+  ) {
+    this.userPipe = new UserPipe(orgService)
+    this.orgPipe = new OrganizationPipe(orgService)
+  }
 
   ngOnInit() {
     this.availableChannelGroups = this.channelGroups;
@@ -35,6 +52,22 @@ export class ChannelGroupsEditComponent implements OnInit, OnDestroy, AfterViewI
 
     this.loading = false;
 
+    const orgSub = this.orgService
+    .getOrganization(this.userService.userOrg)
+    .subscribe((org) => {
+      this.userOrg = org;
+      this.filterOrg();
+    });
+
+    this.subscriptions.add(orgSub);
+
+    this.columns = [
+      {name:'Name', draggable:false,sortable:true },
+      {name:'Description', draggable:false,sortable:true},
+      {name:'# Channels', prop: 'channelIds.length', draggable:false,sortable:true, width: 20},
+      {name:'Owner', prop:'owner', draggable: false, sortable:true, width: 50, pipe: this.userPipe, comparator: this.userComparator.bind(this)},
+      {name:'Org', prop:'orgId', draggable: false, sortable:true, width: 20, pipe: this.orgPipe, comparator: this.orgComparator.bind(this) }
+    ];
   }
 
   ngOnDestroy(): void {
@@ -46,7 +79,7 @@ export class ChannelGroupsEditComponent implements OnInit, OnDestroy, AfterViewI
       this.availableChannelGroups = [...this.availableChannelGroups]; // This is input into <ngx-datatable>
       this.channelTable.recalculate(); // ngx-datatable reference
     }
-
+      // store copy of channel groups
     this.checkValid();
   }
     // onSelect function for data table selection
@@ -92,6 +125,37 @@ export class ChannelGroupsEditComponent implements OnInit, OnDestroy, AfterViewI
       this.subscriptions.add(channelGroupsSub);
     }
 
+  }
+
+  filterOrg() {
+    // filter our data
+    this.availableChannelGroups = this.channelGroups.filter((cg) => {
+      return this.showOnlyUserOrg ? cg.orgId === this.userOrg.id : true;
+    });
+  }
+
+  userComparator(userIdA, userIdB) {
+    const userNameA = this.userPipe.transform(userIdA).toLowerCase();
+    const userNameB = this.userPipe.transform(userIdB).toLowerCase();
+
+    if(userNameA < userNameB) {
+      return -1;
+    }
+    if(userNameA > userNameB) {
+      return 1;
+    }
+  }
+
+  orgComparator(orgIdA, orgIdB) {
+    const orgNameA = this.orgPipe.transform(orgIdA).toLowerCase();
+    const orgNameB = this.orgPipe.transform(orgIdB).toLowerCase();
+
+    if(orgNameA < orgNameB) {
+      return -1;
+    }
+    if(orgNameA > orgNameB) {
+      return 1;
+    }
   }
 
 }
