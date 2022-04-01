84ab97774e7b9585e30b120d2b0b98ae0e89873c
Kyla Marczewski <marczk@uw.edu>
2022-04-01 14:45:30 -0700

    authguard mocks

--- src/app/core/components/not-found/not-found.component.spec.ts
+++ src/app/core/components/not-found/not-found.component.spec.ts
@@ -1,4 +1,4 @@
-import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockInstance, MockRender, ngMocks } from 'ng-mocks';
 
