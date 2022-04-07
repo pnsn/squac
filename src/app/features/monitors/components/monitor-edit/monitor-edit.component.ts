0bf0fd60c0fab98ff5855fbc2aa4a57a3767b55c
Kyla Marczewski <marczk@uw.edu>
2022-04-06 19:06:50 -0700

    update form

--- src/app/features/monitors/components/monitor-edit/monitor-edit.component.ts
+++ src/app/features/monitors/components/monitor-edit/monitor-edit.component.ts
@@ -1,3 +1,4 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
@@ -18,7 +19,12 @@ import { MonitorEditEntryComponent } from '../monitor-edit-entry/monitor-edit-en
   templateUrl: './monitor-edit.component.html',
   styleUrls: ['./monitor-edit.component.scss'],
   providers: [
-    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {hideRequiredMarker: true}}]
+    // {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {hideRequiredMarker: true}},
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: {displayDefaultIndicatorType: false},
+    }
+  ]
 })
 export class MonitorEditComponent implements OnInit, OnDestroy {
   subscriptions: Subscription = new Subscription();
@@ -26,7 +32,6 @@ export class MonitorEditComponent implements OnInit, OnDestroy {
   editMode: boolean;
   orgId: number;
   monitor: Monitor;
-  monitorForm: FormGroup;
   channelGroups: ChannelGroup[];
   metrics: Metric[];
   emailValidator: Validators = Validators.pattern(/^([\w+-.%]+@[\w-.]+\.[A-Za-z]{2,4},?)+$/)
@@ -69,18 +74,18 @@ export class MonitorEditComponent implements OnInit, OnDestroy {
   selectedChannelGroup: ChannelGroup;
   selectedMetric: Metric;
 
+  monitorForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    intervalCount: ['', [ Validators.required, Validators.min(1)]],
+    intervalType: ['', Validators.required],
+    stat: ['', Validators.required],
+    metric: ['', Validators.required],
+    channelGroup: ['', Validators.required],
+    triggers: this.formBuilder.array([])
+  });
+
   // Set up form fields
   ngOnInit() {
-    this.monitorForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      intervalCount: ['', [ Validators.required, Validators.min(1)]],
-      intervalType: ['', Validators.required],
-      stat: ['', Validators.required],
-      metric: ['', Validators.required],
-      channelGroup: ['', Validators.required],
-      triggers: this.formBuilder.array([])
-    });
-
     this.metrics = this.data.metrics;
     this.channelGroups = this.data.channelGroups;
     this.editMode = !!this.data.monitor;
@@ -106,16 +111,39 @@ export class MonitorEditComponent implements OnInit, OnDestroy {
       value_operator: [trigger ? trigger.value_operator : null, Validators.required],
       num_channels:[trigger ? trigger.num_channels : null],
       num_channels_operator:[trigger ? trigger.num_channels_operator : null, Validators.required],
-      alert_on_out_of_alarm: [trigger ? trigger.alert_on_out_of_alarm : null ],
+      alert_on_out_of_alarm: [trigger ? trigger.alert_on_out_of_alarm : false],
       email_list:[trigger ? trigger.email_list : null, this.emailValidator]
     });
   }
 
   // Add a new trigger
   addTrigger(trigger?: Trigger) {
-    this.triggers.push(this.makeTriggerForm(trigger));
+    const triggerFormGroup = this.makeTriggerForm(trigger);
+    triggerFormGroup.valueChanges.subscribe(value => this.validateTrigger(value, triggerFormGroup))
+    this.triggers.push(triggerFormGroup);
   }
 
+  validateTrigger(values, triggerFormGroup) {
+    const val2 = triggerFormGroup.get('val2');
+    const num_channels = triggerFormGroup.get('num_channels');
+    if(values.value_operator !== 'outsideof' && values.value_operator !== 'within') {
+      val2.setValue("", {emitEvent: false});
+      val2.disable({emitEvent: false});
+      val2.removeValidators(Validators.required, {emitEvent: false});
+    } else {
+      val2.addValidators(Validators.required, {emitEvent: false});
+      val2.enable({emitEvent: false});
+    }
+    if(values.num_channels_operator === 'any') {
+      num_channels.setValue("", {emitEvent: false});
+      num_channels.disable({emitEvent: false});
+      num_channels.removeValidators(Validators.required, {emitEvent: false});
+    } else {
+      num_channels.enable({emitEvent: false});
+      num_channels.addValidators(Validators.required, {emitEvent: false});
+    }
+    console.log(values);
+  }
   // Remove given trigger
   removeTrigger(index){
     const trigger = this.triggers.at(index).value;
