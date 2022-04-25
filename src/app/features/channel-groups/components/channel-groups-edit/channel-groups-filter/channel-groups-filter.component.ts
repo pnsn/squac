a5c0ca69c939eb6583e08e726002e2627a124c70
Kyla Marczewski <marczk@uw.edu>
2022-04-25 12:03:45 -0700

    Update cg creation regex to allow ?

--- src/app/features/channel-groups/components/channel-groups-edit/channel-groups-filter/channel-groups-filter.component.ts
+++ src/app/features/channel-groups/components/channel-groups-edit/channel-groups-filter/channel-groups-filter.component.ts
@@ -15,9 +15,10 @@ export class ChannelGroupsFilterComponent {
   };
 
   addFilter(event: any, type: string): void {
-    const value = event.target.value.toLowerCase();
+    const value: string = event.target.value.toLowerCase();
     if (value) {
-      this.filters[type] = value.trim();
+      const filterStr = value.trim().replace(/\?/g, "."); //turn back to acceptable
+      this.filters[type] = `^${filterStr}$`;
     } else {
       this.filters[type] = "";
     }
