43ac5ab1b5814a4e2f46945727f129844964a758
Kyla Marczewski <marczk@uw.edu>
2022-05-24 14:59:27 -0700

    change shared loading

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "shared-loading",
-  template: '<div id="loading-container"><span>loading...<span><div>',
+  templateUrl: "./loading.component.html",
   styleUrls: ["./loading.component.scss"],
 })
 export class LoadingComponent {}
