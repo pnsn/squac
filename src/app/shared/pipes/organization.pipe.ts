b9fd3177d1d5b3739b6339d81e69502aea13c592
Kyla Marczewski <marczk@uw.edu>
2022-09-09 16:43:37 -0700

    fix strings for user

--- src/app/shared/pipes/organization.pipe.ts
+++ src/app/shared/pipes/organization.pipe.ts
@@ -8,6 +8,9 @@ export class OrganizationPipe implements PipeTransform {
   constructor(private orgService: OrganizationService) {}
 
   transform(value: number): string {
+    if (typeof value === "string") {
+      return value;
+    }
     return this.orgService.getOrgName(value);
   }
 }
--- src/app/shared/pipes/user.pipe.ts
+++ src/app/shared/pipes/user.pipe.ts
@@ -8,6 +8,9 @@ export class UserPipe implements PipeTransform {
   constructor(private orgService: OrganizationService) {}
 
   transform(value: number): string {
+    if (typeof value === "string") {
+      return value;
+    }
     return this.orgService.getOrgUserName(value);
   }
 }
