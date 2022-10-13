57a5c5584f6c53ba6f31a35c4c73284b006de9bf
Kyla Marczewski <marczk@uw.edu>
2022-10-13 15:20:15 -0700

    linting

--- src/app/features/widget/services/aggregate.service.spec.ts
+++ src/app/features/widget/services/aggregate.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { AggregateService } from './aggregate.service';
+import { AggregateService } from "./aggregate.service";
 
-describe('AggregateService', () => {
+describe("AggregateService", () => {
   let service: AggregateService;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('AggregateService', () => {
     service = TestBed.inject(AggregateService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
