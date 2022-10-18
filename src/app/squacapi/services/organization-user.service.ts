d8b9a8357bf8536d3647fd87bfd6f6ff1953440c
Kyla Marczewski <marczk@uw.edu>
2022-10-17 17:27:57 -0700

    cleanup

--- src/app/squacapi/services/organization-user.service.ts
+++ src/app/squacapi/services/organization-user.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "@pnsn/ngx-squacapi-client";
 import { ReadWriteDeleteApiService } from "@squacapi/interfaces/generic-api-service";
-import { Organization } from "@squacapi/models/organization";
 import { User, UserAdapter } from "@squacapi/models/user";
-import { Observable, tap } from "rxjs";
 
 @Injectable({
   providedIn: "root",
--- src/app/squacapi/services/organization.service.spec.ts
+++ src/app/squacapi/services/organization.service.spec.ts
@@ -1,16 +1,17 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { TestBed } from "@angular/core/testing";
 
-import { OrganizationService } from './organization.service';
+import { OrganizationService } from "./organization.service";
 
-describe('OrganizationService', () => {
+describe("OrganizationService", () => {
   let service: OrganizationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
     service = TestBed.inject(OrganizationService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
