a543cffcade812b751656d96d21ecc997da3c6a1
Kyla Marczewski <marczk@uw.edu>
2023-01-31 13:34:28 -0800

    fix aggregates/archives not changing

--- projects/widgets/src/lib/widget-types/tabular/tabular.component.ts
+++ projects/widgets/src/lib/widget-types/tabular/tabular.component.ts
@@ -257,6 +257,7 @@ export class TabularComponent
 
           if (data.get(channel.id)) {
             const rowData = data.get(channel.id).get(metric.id);
+            console.log(rowData);
             val = rowData && rowData[0] ? rowData[0].value : null;
           }
 
