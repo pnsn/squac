28c8d2eeba56af1abf70550b6370eaabfd265d95
Kyla Marczewski <marczk@uw.edu>
2023-04-03 14:58:31 -0700

    remove unused imports

--- projects/widgets/src/lib/services/widget-connect.service.ts
+++ projects/widgets/src/lib/services/widget-connect.service.ts
@@ -1,14 +1,10 @@
 import { Injectable } from "@angular/core";
 import {
   debounceTime,
-  distinct,
   distinctUntilChanged,
-  pipe,
   ReplaySubject,
   share,
   Subject,
-  tap,
-  throttleTime,
 } from "rxjs";
 
 /**
