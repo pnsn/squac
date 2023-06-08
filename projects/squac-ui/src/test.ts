ee3ed646cc30af4d9075aa7f42a4d169ae1241ec
Kyla Marczewski <marczk@uw.edu>
2023-06-08 15:58:57 -0700

    uddate angular

--- projects/squac-ui/src/test.ts
+++ projects/squac-ui/src/test.ts
@@ -7,8 +7,6 @@ import {
 } from "@angular/platform-browser-dynamic/testing";
 import { DefaultTitleStrategy, TitleStrategy } from "@angular/router";
 import { MockInstance, MockService, ngMocks } from "ng-mocks";
-
-declare const require: any;
 declare const jasmine: any;
 // console.warn = (message) => {throw new Error(message)};
 // ngMocks.autoSpy("jasmine");
@@ -34,7 +32,3 @@ getTestBed().initTestEnvironment(
     teardown: { destroyAfterEach: true },
   }
 );
-// Then we find all the tests.
-const context = require.context("./", true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
