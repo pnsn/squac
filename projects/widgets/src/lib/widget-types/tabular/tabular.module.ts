d70b898670c898e09445a9dfc43c7a39892d5984
Kyla Marczewski <marczk@uw.edu>
2023-06-13 11:37:50 -0700

    table formatting

--- projects/widgets/src/lib/widget-types/tabular/tabular.module.ts
+++ projects/widgets/src/lib/widget-types/tabular/tabular.module.ts
@@ -4,13 +4,22 @@ import { SharedModule } from "../../shared/shared.module";
 import { MatTableModule } from "@angular/material/table";
 import { MatSortModule } from "@angular/material/sort";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTooltip, MatTooltipModule } from "@angular/material/tooltip";
 
 /**
  *
  */
 @NgModule({
   declarations: [TabularComponent],
-  imports: [MatTableModule, MatSortModule, SharedModule, MatIconModule],
+  imports: [
+    MatTableModule,
+    MatSortModule,
+    SharedModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule,
+  ],
   exports: [TabularComponent],
   providers: [],
 })
